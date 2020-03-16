39ed2a193dfa4f8ebabd0e889fa18ef3595cf40f
nLeul <leulnecha@gmail.com>
2020-03-16 16:47:26 -0500

    added save product

--- controllers/controllers.product.js
+++ controllers/controllers.product.js
@@ -4,3 +4,17 @@ exports.getProdPage = (req, res, next) => {
      res.render('../views/product/add-products',{title:"Add-Products"});
 
 };
+exports.saveProduct = (req, res, next) => {
+    const product = new Product({
+        name: req.body.name,
+        category: req.body.category,
+        price: req.body.price,
+        image: req.body.image,
+        description: req.body.description
+    });
+    product.save()
+        .then((result) => {
+            res.redirect('/savePrd');
+        })
+        .catch(err => console.log(err));
+};
--- routers/routers.product.js
+++ routers/routers.product.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const productController = require('../controllers/controllers.product');
 
 router.get("/savePrd", productController.getProdPage);
+router.post("/save-product", productController.saveProduct);
 
 module.exports = router;
