5009415ddaf88aef723f40e26d11751d5c55b728
Seble Aygoda <saygoda@mum.edu>
2020-03-17 22:43:06 -0500

    add to cart

--- models/models.cart.js
+++ models/models.cart.js
@@ -8,7 +8,7 @@ const cartSchema = new Schema(
             //user obj id
             type:Schema.Types.ObjectId,ref:'User',required:true
         },
-        listOfProds:{
+        listOfProds:{ 
             prdArr:[
                 {
                    prdId:{ type:Schema.Types.ObjectId,ref:'Product'},
@@ -20,35 +20,9 @@ const cartSchema = new Schema(
 );
 
 cartSchema.statics.addToCart = function(userId,prdId,qty){
-        // console.log(userId+" "+prdId+" "+qty);
-     this.findOne({userId:userId})
-     .then(user=>{ console.log(user+"============")
-         if(user){
+         console.log(userId+" "+prdId+" "+qty);
 
-         }
-     })
-     .catch(e=>console.log(e));
+         
 }
 
 module.exports = mongoose.model('Cart', cartSchema);
-//  Cart.findOne({userId:req.session.user._id})
-//     .then(user=>{
-
-//         if(user){
-
-//         }else{
-//             let newCart=new Cart({
-//                 userId:req.session.user.,
-//                 listOfProds:{
-//                     prdArr: prdArr.push({
-//                         prdId: req.body.id,
-//                         quantity: req.body.quantity
-//                     })
-//                 }
-//             }).save();
-//         }
-
-//       })
-//     .catch(err=>{
-//     console.log(err);
-// })
